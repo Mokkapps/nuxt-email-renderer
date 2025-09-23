ec57a15369c10893b1fe65c39eb9cbee42a195c8
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2025-09-23 11:35:29 +0200

    ci: add happy-dom dependency and update vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
+import { defineVitestProject } from '@nuxt/test-utils/config'
 import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
   test: {
-    environment: 'jsdom',
+    projects: [
+      await defineVitestProject({
+        test: {
+          include: ['src/runtime/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+          name: 'module',
+        },
+      }),
+    ],
   },
 })
